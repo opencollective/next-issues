990c9f805ef6b7cad752f120db2fac944328b9e3
François Hodierne <francois@hodierne.net>
2018-06-14 21:51:00 +0200

    style-css-404-now-production: Surface a bug with Next.js and Next CSS where style.css is 404 when using now.

---
+++ style-css-404-now-production/pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+
+import '../css/document.css';
+
+export default class MyDocument extends Document {
+
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <link rel="stylesheet" href="/_next/static/style.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
---
+++ style-css-404-now-production/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Index extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <div className="shouldBeGreen">
+          This should be green.
+        </div>
+      </div>
+    );
+  }
+
+}
